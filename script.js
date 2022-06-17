e108e349d5bed66774ada87959a45a73033a97f7
nicofrancis <nicolofrancis10@gmail.com>
2022-06-17 14:41:10 -0600

    Pixelate

--- script.js
+++ script.js
@@ -15,7 +15,14 @@ makeRow();
 
 const button = document.getElementById("add-row");
 button.addEventListener("click", makeRow);
+
 table.addEventListener("click", colorize);
-function colorize(colorize) {
-  console.log("clicked");
+
+function colorize(event) {
+  const target = event.target;
+  if (target.className.length) {
+    target.className = "";
+  } else {
+    target.className = "red";
+  }
 }
